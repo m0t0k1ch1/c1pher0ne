ccc2a4ae7ff0d109da68582f27f9fbbebe3fbaff
Sato Motoki <m0t0k1ch1.310@gmail.com>
2013-09-26 03:52:37 +0900

    fix schema role

--- lib/Cipherone/Role/Schema.pm
+++ lib/Cipherone/Role/Schema.pm
@@ -5,8 +5,12 @@ use Cipherone::Schema;
 use Cipherone::Schema::Adjective;
 use Cipherone::Schema::Country;
 use Cipherone::Schema::MediaCategory;
+use Cipherone::Schema::MediaRanking;
+use Cipherone::Schema::MediaRankingDetail;
 use Cipherone::Schema::MediaType;
 use Cipherone::Schema::Trend;
+use Cipherone::Schema::TrendDetail;
+use Cipherone::Schema::TrendSource;
 
 sub schema {
     my ($self, $schema) = @_;
