55ecae651882ea18a676f0779fe4c53c718e52d9
Sato Motoki <m0t0k1ch1.310@gmail.com>
2013-09-30 01:16:34 +0900

    update db

--- script/dev/create_tables.pl
+++ script/dev/create_tables.pl
@@ -24,7 +24,7 @@ $teng->do(q{
 $teng->do(q{
     CREATE TABLE media_category (
         id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        im_id SMALLINT UNSIGNED NOT NULL,
+        im_id INT UNSIGNED NOT NULL,
         name TEXT NOT NULL,
         url TEXT NOT NULL
     )
@@ -44,13 +44,14 @@ $teng->do(q{
         id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
         media_ranking_id INT UNSIGNED NOT NULL,
         media_category_id INT UNSIGNED NOT NULL,
-        rank SMALLINT UNSIGNED NOT NULL,
+        rank INT UNSIGNED NOT NULL,
         im_id BIGINT UNSIGNED NOT NULL,
         title TEXT NOT NULL,
         artist TEXT NOT NULL,
         url TEXT NOT NULL,
         image_url TEXT NOT NULL,
-        release_date DATETIME NOT NULL
+        release_date DATETIME NOT NULL,
+        is_tweet TINYINT UNSIGNED NOT NULL DEFAULT 0
     )
 });
 
@@ -73,7 +74,8 @@ $teng->do(q{
     CREATE TABLE trend_detail (
         id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
         trend_id INT UNSIGNED NOT NULL,
-        body TEXT NOT NULL
+        body TEXT NOT NULL,
+        is_tweet TINYINT UNSIGNED NOT NULL DEFAULT 0
     )
 });
 
