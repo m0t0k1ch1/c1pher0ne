16c3bb2203d957e6f93ec93ea7976e3930d8a294
Sato Motoki <m0t0k1ch1.310@gmail.com>
2013-09-30 02:35:21 +0900

    add bitly role

---
+++ lib/Cipherone/Role/Bitly.pm
@@ -0,0 +1,22 @@
+package Cipherone::Role::Bitly;
+use Mouse::Role;
+
+use WebService::Bitly;
+
+has bitly => (
+    is         => 'rw',
+    lazy_build => 1,
+);
+
+sub _build_bitly {
+    my $self = shift;
+
+    my $bitly_config = $self->_config->{bitly};
+
+    WebService::Bitly->new(
+        user_name    => $bitly_config->{user_name},
+        user_api_key => $bitly_config->{api_key},
+    );
+}
+
+1;
