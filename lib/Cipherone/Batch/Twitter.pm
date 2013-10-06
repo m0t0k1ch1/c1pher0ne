6636acdd2496bf93d2843b95168ec65562c920cc
Sato Motoki <m0t0k1ch1.310@gmail.com>
2013-10-06 11:33:52 +0900

    update batches

--- lib/Cipherone/Batch/Twitter.pm
+++ lib/Cipherone/Batch/Twitter.pm
@@ -8,37 +8,9 @@ use Encode;
 use File::Basename;
 use LWP::UserAgent;
 
-has image_on => (
-    is      => 'ro',
-    default => 'static/img/c1pher0ne_on.png',
-);
-
-has image_off => (
-    is      => 'ro',
-    default => 'static/img/c1pher0ne_off.png',
-);
-
 __PACKAGE__->meta->make_immutable;
 
 no Mouse;
-
-sub change_image_on {
-    my $self = shift;
-
-    $self->cipherone->twitter->update_profile_image([$self->image_on]);
-}
-
-sub change_image_off {
-    my $self = shift;
-
-    $self->cipherone->twitter->update_profile_image([$self->image_off]);
-}
-
-sub tweet {
-    my ($self, $body) = @_;
-
-    $self->cipherone->twitter->update($body);
-}
 
 sub tweet_trend {
     my $self = shift;
